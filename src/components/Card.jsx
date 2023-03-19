afb3e67f4a51c0e46c628ba4e7785c47d0d036f3
Ahsaan <ahsaanjee045@gmail.com>
2023-03-19 18:55:19 +0530

    added margin around the image cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { downloadImage } from "../utils";
 
 const Card = ({ _id, name, prompt, photo }) => {
   return (
-    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
+    <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card m-1">
       <img
         src={photo}
         alt={prompt}
